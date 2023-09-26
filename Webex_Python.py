93c8189872ae56856afeca8a124aba97f3f5cd40
michael Becze <michael.becze@gmail.com>
2023-09-26 06:35:34 -0400

    added messages

--- Webex_Python.py
+++ Webex_Python.py
@@ -8,15 +8,25 @@ token = 'ZjQ4NTk4MjUtZjJkMy00YTkzLWExNzEtYmM4MGM3YTI4YTg4NzUyYzE0OGItYWFh_P0A1_b
 headers = {'Authorization' : f'Bearer {token}',
            'Content-Type' : 'application/json'}
 
-def Create_Room(*arg):
+def Create_Room(arg):
     body = {
     "title": str(arg)}
     post_room = requests.post(url='https://webexapis.com/v1/rooms', headers=headers, data=json.dumps(body))
     print(post_room)
     
                               
-                            
+def Message(arg1):
+    body = {
+    "roomId" : 'Y2lzY29zcGFyazovL3VybjpURUFNOnVzLXdlc3QtMl9yL1JPT00vYzc2ZjdmNjAtNWM1Ni0xMWVlLWJjMTctYmQwYmM4Nzk0N2Q4', 
+    "text" : str(arg1)}
+    post_room = requests.post(url='https://webexapis.com/v1/messages', headers=headers, data=json.dumps(body))
+    print(post_room)
+
+
+
+Message('First Message')
+
 #Create_Room()
 
-list_rooms = requests.get(url='https://webexapis.com/v1/rooms', headers=headers)
-print(list_rooms.text)
+#list_rooms = requests.get(url='https://webexapis.com/v1/rooms', headers=headers)
+#print(list_rooms.text)
